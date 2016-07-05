e5fc5ab97d9c7b8e0f7034d435a05a2b60f39d1d
Yordanos <yordanosd@gmail.com>
2016-07-05 09:33:40 -0700

    got tic tac toe to work

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,7 +1,102 @@
-function TicTacToe() {
+function TicTacToe(player1, player2) {
+  this._player1 = player1 //even +1
+  this._player2 = player2 //odd -1
+  this._turn = 0
+  this._board = [[0,0,0,], [0,0,0], [0,0,0]]
 
 }
+
+//if player one plays = 1 and if players 2 plays dd -1
 
 TicTacToe.prototype = {
-  
+  position_empty: function(current_position){
+    return current_position === 0
+  },
+
+  play: function(row, col){
+
+    var current_position = this._board[row][col]
+
+    if(this.position_empty(current_position)){
+      if(this._turn % 2 == 0 ){
+        this._board[row][col] = 1
+        var playerClass = "player1"
+      }else{
+        this._board[row][col] = -1
+        var playerClass = "player2"
+      }
+      this._turn ++
+      return playerClass
+
+    }else{
+      console.log(this.position_empty(current_position))
+      console.log("Please try again")
+    }
+  },
+
+  checkWin: function(){
+    result = []
+    for (i = 0; i<3; i++){
+      result.push(this._board[0][i] + this._board[1][i] + this._board[2][i])
+      result.push(this._board[i][0] + this._board[i][1] + this._board[i][2])
+    }
+      result.push(this._board[0][0] + this._board[1][1] + this._board[2][2])
+      result.push(this._board[0][2] + this._board[1][1] + this._board[2][0])
+
+
+    if(result.includes(3)){
+      return "player1Wins"
+    } else if (result.includes(-3)) {
+      return "player2Wins"
+    }else if (this._turn > 8) {
+      return "draw"
+    }else{
+      return false
+    }
+  }
+
 }
+
+$(document).ready(function(){
+  var game = new TicTacToe("player1", "player2")
+  var table = $('table')
+  var buttons = $('.board button')
+  var reset = $('#reset')
+  console.log(buttons)
+
+  // table.addClass('player1Wins');
+
+  buttons.on('click',function(event){
+    var row = $(this).data('row')
+    var col = $(this).data('col')
+
+    var newClass = game.play(row, col)
+
+    // if(!(game.checkWin())){
+    //   $(this).addClass(newClass)
+    // }else{
+    //   $(this).addClass(newClass)
+    //   buttons.addClass(game.checkWin());
+    // }
+
+// false == NO winner
+
+// true  === Winner or Draw
+
+
+    if (game.checkWin()){
+      console.log("game winnner " + game.checkWin())
+
+      $(this).addClass(newClass);
+      buttons.addClass(game.checkWin());
+    }else {
+      console.log("try again " + game.checkWin())
+
+      $(this).addClass(newClass);
+    }
+
+  });
+
+
+
+});
---
+++ turns.md
@@ -0,0 +1,13 @@
+# What constitutes a turn?
+
+#- determine player turn
+#- select a space
+  - space available?
+  - space advantageous?
+#- claim space using player identifier
+#- check win/loss/draw state
+  - what is a win?
+  - what is a draw?
+  - what is a loss?
+  - communicate outcome
+#- continue player?
